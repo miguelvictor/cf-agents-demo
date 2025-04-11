2259addc7aa34cb4213ff5e3ddd46450f977bb2a
Miguel Victor <miguel@digitalfans.se>
2025-04-11 09:42:17 +0200

    added prettier

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,20 +1,19 @@
-import { Button } from "~/components/ui/button";
-import type { Route } from "./+types/_index";
-import { useState } from "react";
+import { useState } from "react"
+
+import { Button } from "~/components/ui/button"
+
+import type { Route } from "./+types/_index"
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "CF Agents Demo" },
-    { name: "description", content: "CF Agents Demo" },
-  ];
+  return [{ title: "CF Agents Demo" }, { name: "description", content: "CF Agents Demo" }]
 }
 
 export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
+  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE }
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <main className="p-20">
@@ -24,5 +23,5 @@ export default function Home({ loaderData }: Route.ComponentProps) {
       </Button>
       {count > 0 && <p className="mt-4">You clicked {count} times</p>}
     </main>
-  );
+  )
 }
